6b9acd942ca71a4903c958752fea990c0d219c48
joressHACK24 <joresfotso01.gmail.com>
2026-06-05 01:00:53 +0200

    feat: Multivariaten Gradientenabstieg mit Z-Score Standardisierung implementiert

--- 31.05_gradient _descent.py
+++ 31.05_gradient _descent.py
@@ -1,4 +1,4 @@
-import numpy as np
+'''import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -36,6 +36,49 @@ def batch_gradient_descent(X, y, learning_rate, epochs):
     
     plt.show()
 
-batch_gradient_descent(X,y,0.01,1000)
+batch_gradient_descent(X,y,0.01,1000)'''
+
+#_______________________________________________________________________________________________________________________
+
+import numpy as np
+
+# X = [Surface (m2), Nombre de chambres]
+X = np.array([
+    [50, 1],
+    [80, 2],
+    [120, 3],
+    [150, 3],
+    [200, 4]
+])
+
+# y = Prix (en milliers d'euros)
+y = np.array([150, 250, 350, 420, 600])
+
+w = np.zeros(X.shape[1])
+b = 0
+
+m = len(X)
+epochs = 100
+learning_rate = 0.01
 
 
+def standardisation(X):
+    x_neu = np.array([[]])
+    for i in range(m):
+        
+        u = np.mean(X)
+        o = np.std(X)
+        x_neu = (X - u)/o
+    return x_neu
+
+X_neu = standardisation(X)
+print(X_neu)
+for i in range(epochs):
+    y_pred = X_neu @ w + b
+
+    w -= learning_rate * (1/m) * (mplement multivariate gradient descent with Z-score(X_neu.T) @(y_pred - y))
+    b -= learning_rate * (1/m) * np.sum((y_pred - y))
+
+
+print(w)
+print(b)
